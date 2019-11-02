94d7e7f1a4f97eda899760a19ef0c70483aa1262
Heung Park <hpark327@uwo.ca>
2019-11-02 12:40:00 -0400

    Designed tab to take up minimum space, by showing current tab in greyed out dot

--- mobileApp/app/src/main/java/com/heung/forme/MainActivity.java
+++ mobileApp/app/src/main/java/com/heung/forme/MainActivity.java
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
             SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
             ViewPager viewPager = findViewById(R.id.view_pager);
             viewPager.setAdapter(sectionsPagerAdapter);
-            TabLayout tabs = findViewById(R.id.tabs);
-            tabs.setupWithViewPager(viewPager);
+            TabLayout tabLayout = findViewById(R.id.tabDots);
+            tabLayout.setupWithViewPager(viewPager, true);
             FloatingActionButton fab = findViewById(R.id.fab);
 
             fab.setOnClickListener(new View.OnClickListener() {
--- mobileApp/app/src/main/java/com/heung/forme/ui/main/SectionsPagerAdapter.java
+++ mobileApp/app/src/main/java/com/heung/forme/ui/main/SectionsPagerAdapter.java
@@ -16,13 +16,8 @@ import com.heung.forme.R;
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
-    private final Context mContext;
-
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
-        mContext = context;
     }
 
     @Override
@@ -30,12 +25,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
         return PlaceholderFragment.newInstance(position + 1);
-    }
-
-    @Nullable
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
     }
 
     @Override
